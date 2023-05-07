8e0404f85597d4b55d9706796c409377b3debca5
alamin <alaminbhuyan322@gmail.com>
2023-05-07 17:47:09 +0600

    done all the thing

--- curd_app/views.py
+++ curd_app/views.py
@@ -12,11 +12,14 @@ def home(request):
 # @csrf_exempt
 def save_info(request):
     if request.method == 'POST':
+        stuid = request.POST.get('id')
         name = request.POST['name']
         email = request.POST['email']
         password = request.POST['password']
-        
-        user = UserInfo(name=name, email=email, password=password)
+        if stuid == "":
+            user = UserInfo(name=name, email=email, password=password)
+        else:
+            user = UserInfo(id=stuid, name=name, email=email, password=password)
         user.save()
         stud = UserInfo.objects.values()
         print(stud)
@@ -27,7 +30,13 @@ def save_info(request):
 
 
 def update_info(request):
-    pass
+    if request.method == 'POST':
+        id = request.POST['sid']
+        user = UserInfo.objects.get(pk=id)
+        user_data = {'id':user.id, 'name': user.name, 'email': user.email, 'password': user.password}
+        return JsonResponse(user_data)
+    else:
+        JsonResponse({'status': 0})
 
 def delete_info(request):
     if request.method == 'POST':
